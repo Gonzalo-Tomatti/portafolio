814acb49210ea266a4cd7663ba3e58b87f208175
Gonzalo Tomatti <gonzalo98.g@hotmail.com>
2022-12-03 12:18:40 -0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,13 @@ const About = () => {
     <div className="container text-center my-5">
       <h1 className="">Sobre mí</h1>
       <p>
-        Mi nombre es Gonzalo y vivo en City Bell, Buenos Aires. Actualmente
-        programo websites usando el stack MERN. Desde el momento en que imprimí
-        "Hello World" en la pantalla de mi primera aplicación, empezó a crecer
-        mi pasión por el mundo de la programación. Pero el desarrollo de
-        software no es solo un hobby para mí, sino que ofrece un desafío de
-        aprender y mejorar mis habilidades continuamente para crear software de
-        alta calidad. Espero que mis trabajos te transmitan la pasión que tengo
-        por lo que hago.
+        Mi nombre es Gonzalo y vivo en City Bell, Buenos Aires. Desde el momento
+        en que imprimí "Hello World" en la pantalla de mi primera aplicación,
+        empezó a crecer mi pasión por el mundo de la programación. Pero el
+        desarrollo de software no es solo un hobby para mí, sino que ofrece un
+        desafío de aprender y mejorar mis habilidades continuamente para crear
+        software de alta calidad. Espero que mis trabajos te transmitan la
+        pasión que tengo por lo que hago.
       </p>
       <p>Tecnologías y herramientas que uso:</p>
     </div>
